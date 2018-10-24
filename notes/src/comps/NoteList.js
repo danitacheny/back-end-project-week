15a6feb7f0bec8b40351b05b72dc7b26afe20560
Dani Tacheny <dani.tacheny@gmail.com>
2018-10-23 20:33:33 -0500

    change NoteList component to class component
    if the notes in redux store is empty, try doing a fetch

--- notes/src/comps/NoteList.js
+++ notes/src/comps/NoteList.js
@@ -7,56 +7,65 @@ import { selectNote, sortNotes, fetchNotes } from '../actions';
 import '../styles/NoteList.css';
 
 // props include the list of notes from App.js
-const NoteList = props => {
-  return (
-    <div className="noteList__container">
-      <h1 className="notesList__header">Your Notes:</h1>
-      <CSVLink
-        data={props.notes}
-        filename={'my-notes.csv'}
-        className="noteList__download"
-      >
-        download
-      </CSVLink>
-      <div className="noteList__sort-options">
-        <div className="noteList__sort-title">sort by:</div>
-        <div
-          onClick={() => props.sortNotes('date')}
-          className="noteList__sort-option"
-        >
-          date
-        </div>
-        <div
-          onClick={() => props.sortNotes('title')}
-          className="noteList__sort-option"
-        >
-          name
-        </div>
-      </div>
-      <div className="noteList">
-        {props.notes.map(note => {
-          return (
-            <div key={note._id} className="notelist__item">
-              <Link
-                to={`/view/${note._id}`}
-                onClick={() => props.selectNote(note._id)}
-              >
-                <NoteThumb note={note} />
-              </Link>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
-};
+class NoteList extends React.Component {
+  componentDidMount = () => {
+    if (!this.props.notes.length) {
+      this.props.fetchNotes();
+    }
+  };
 
-const mapStateToProps = state => {
+  render() {
+    return (
+      <div className="noteList__container">
+        <h1 className="notesList__header">Your Notes:</h1>
+        <CSVLink
+          data={this.props.notes}
+          filename={'my-notes.csv'}
+          className="noteList__download"
+        >
+          download
+        </CSVLink>
+        <div className="noteList__sort-options">
+          <div className="noteList__sort-title">sort by:</div>
+          <div
+            onClick={() => this.props.sortNotes('date')}
+            className="noteList__sort-option"
+          >
+            date
+          </div>
+          <div
+            onClick={() => this.props.sortNotes('title')}
+            className="noteList__sort-option"
+          >
+            name
+          </div>
+        </div>
+        <div className="noteList">
+          {this.props.notes.map((note) => {
+            return (
+              <div key={note._id} className="notelist__item">
+                <Link
+                  to={`/view/${note._id}`}
+                  onClick={() => this.props.selectNote(note._id)}
+                >
+                  <NoteThumb note={note} />
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
   return {
     notes: state.notes,
   };
 };
 
-export default connect(mapStateToProps, { selectNote, sortNotes, fetchNotes })(
-  NoteList
-);
+export default connect(
+  mapStateToProps,
+  { selectNote, sortNotes, fetchNotes }
+)(NoteList);
